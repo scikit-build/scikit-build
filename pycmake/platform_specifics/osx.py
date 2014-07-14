fd9b59ef132e08e3cdb964755c9baabc8435c3ee
Mike Sarahan <msarahan@gmail.com>
2014-07-13 20:24:55 -0700

    Add Unix Makefiles as default for Linux/OSX

--- pycmake/platform_specifics/osx.py
+++ pycmake/platform_specifics/osx.py
@@ -3,3 +3,4 @@ import abstract
 class OSXPlatform(abstract.CMakePlatform):
     def __init__(self):
         super(OSXPlatform, self).__init__()
+        self.default_generators = ["Unix Makefiles", ]
