411a6d8f6a87229a1c115a320d3dd58e5aab5eff
Omar Padron <omar.padron@kitware.com>
2016-03-17 12:01:53 -0400

    reorganize platform-specific code
      - use case-insensitive string comparison when detecting platform
      - factor out logic common across unices into unix.py
      - add new platform: "freebsd"

---
+++ skbuild/platform_specifics/unix.py
@@ -0,0 +1,8 @@
+from . import abstract
+
+
+class UnixPlatform(abstract.CMakePlatform):
+
+    def __init__(self):
+        super(UnixPlatform, self).__init__()
+        self.default_generators = ["Unix Makefiles", ]
