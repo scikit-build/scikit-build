842a6d7e3a12f096ff4debbb786c0559f8c21099
Omar Padron <omar.padron@kitware.com>
2016-05-03 17:46:00 -0400

    unescape the backslashes in run-with-mingw.cmd

--- ci/appveyor/run-with-mingw.cmd
+++ ci/appveyor/run-with-mingw.cmd
@@ -10,14 +10,12 @@ SET PATH=%MINGW_ROOT%\bin;%PATH%
 
 :: workaround for CMake not wanting sh.exe on PATH for MinGW
 
-set PATH=%PATH:C:\\Program Files\\Git\\usr\\bin;=%
-set PATH=%PATH:C:\\Program Files (x86)\\Git\\usr\\bin;=%
-set PATH=%PATH:C:\\Program Files\\Git\\bin;=%
-set PATH=%PATH:C:\\Program Files (x86)\\Git\\bin;=%
-set PATH=%PATH:C:\\Program Files\\Git\\cmd;=%
-set PATH=%PATH:C:\\Program Files (x86)\\Git\\cmd;=%
-
-echo PATH: %PATH%
+set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
+set PATH=%PATH:C:\Program Files (x86)\Git\usr\bin;=%
+set PATH=%PATH:C:\Program Files\Git\bin;=%
+set PATH=%PATH:C:\Program Files (x86)\Git\bin;=%
+set PATH=%PATH:C:\Program Files\Git\cmd;=%
+set PATH=%PATH:C:\Program Files (x86)\Git\cmd;=%
 
 
 ECHO Executing: %COMMAND_TO_RUN%
