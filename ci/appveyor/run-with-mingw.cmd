3984b0b14e8b8e5185229b2ad68abe2e9c377546
Omar Padron <omar.padron@kitware.com>
2016-05-03 17:17:38 -0400

    remove sh.exe from PATH only when running with MinGW

--- ci/appveyor/run-with-mingw.cmd
+++ ci/appveyor/run-with-mingw.cmd
@@ -6,6 +6,18 @@ SET MINGW_ROOT=C:\MinGW
 
 ECHO Using default MinGW build environment
 SET PATH=%MINGW_ROOT%\bin;%PATH%
+
+
+REM workaround for CMake not wanting sh.exe on PATH for MinGW
+
+set PATH=%PATH:C:\\Program Files\\Git\\usr\\bin;=%
+set PATH=%PATH:C:\\Program Files (x86)\\Git\\usr\\bin;=%
+set PATH=%PATH:C:\\Program Files\\Git\\bin;=%
+set PATH=%PATH:C:\\Program Files (x86)\\Git\\bin;=%
+set PATH=%PATH:C:\\Program Files\\Git\\cmd;=%
+set PATH=%PATH:C:\\Program Files (x86)\\Git\\cmd;=%
+
+
 ECHO Executing: %COMMAND_TO_RUN%
 call %COMMAND_TO_RUN% || EXIT 1
 
