db7dd4b8edde3694706daeb3bb89c10f826a7bb1
Omar Padron <omar.padron@kitware.com>
2016-05-06 15:45:34 -0400

    fix #28 -- update visual studio version mappings in windows platform

--- skbuild/platform_specifics/windows.py
+++ skbuild/platform_specifics/windows.py
@@ -4,31 +4,46 @@ import platform
 # base class functionality
 from . import abstract
 
-
 class WindowsPlatform(abstract.CMakePlatform):
 
     def __init__(self):
         super(WindowsPlatform, self).__init__()
         self.default_generators = ["MinGW Makefiles", ]
-        python_major_version = sys.version_info[0]
-        # python 3 from the mothership uses VS 2010
-        if python_major_version == 3:
-            vs_base = "Visual Studio 10"
-        # python 2 from the mothership uses VS 2008
-        elif python_major_version == 2:
+        version = sys.version_info
+
+        # For Python 2.7 to Python 3.2: VS2008
+        if (
+            (version.major == 2 and version.minor >= 7) or
+            (version.major == 3 and version.minor <  3)
+        ):
             vs_base = "Visual Studio 9 2008"
+
+        # For Python 3.3 to Python 3.4: VS2010
+        elif (
+            version.major == 3 and (
+                version.minor >= 3 and
+                version.minor <  5
+            )
+        ):
+            vs_base = "Visual Studio 10 2010"
+
+        # For Python 3.5 and above: VS2015
+        elif version.major == 3 and version.minor >= 5:
+            vs_base = "Visual Studio 14 2015"
+
         else:
-            raise RuntimeError("Only Python 2 and 3 are supported - "
-                               "please add support in platform_specific "
-                               "scikit-build folder")
+            raise RuntimeError("Only Python >= 2.7 is supported on Windows.")
 
         # Python is Win64, build a Win64 module
         if platform.architecture() == "x64":
             vs_base += " Win64"
+
         # only VS 11 and above support ARM, but include it here in hopes of
         # making future work easier.
         elif platform.architecture() == "ARM":
             vs_base += " ARM"
+
         # we're implicitly doing nothing for 32-bit builds.  Their generator
         # string IDs seem to be just the vs_base.
+
         self.default_generators.insert(0, vs_base)
