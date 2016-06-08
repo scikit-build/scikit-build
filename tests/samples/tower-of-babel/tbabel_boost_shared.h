ac5d0e8c12a5d161ebc1f3246a65810a1c1ce8d6
Omar Padron <omar.padron@kitware.com>
2016-06-08 16:44:59 -0400

    clean up tower-of-babel boost module header files

--- tests/samples/tower-of-babel/tbabel_boost_shared.h
+++ tests/samples/tower-of-babel/tbabel_boost_shared.h
@@ -2,12 +2,9 @@
 #ifndef _TBABEL_BOOST_SHARED_H
 #define _TBABEL_BOOST_SHARED_H
 
-#include <boost/python.hpp>
-
-typedef void (*cy_routine)(unsigned int, unsigned int,
-                           PyObject *, PyObject *);
-
-extern void tb_shared_cycle(unsigned int, unsigned int, PyObject *, PyObject *);
+#define TBABEL_MODE       SHARED
+#define TBABEL_MODE_LOWER shared
+#include <tbabel_boost_common.h>
 
 #endif /* !_TBABEL_BOOST_SHARED_H */
 
--- tests/samples/tower-of-babel/tbabel_boost_static.h
+++ tests/samples/tower-of-babel/tbabel_boost_static.h
@@ -2,7 +2,8 @@
 #ifndef _TBABEL_BOOST_STATIC_H
 #define _TBABEL_BOOST_STATIC_H
 
-#define MODE STATIC
+#define TBABEL_MODE       STATIC
+#define TBABEL_MODE_LOWER static
 #include <tbabel_boost_common.h>
 
 #endif /* !_TBABEL_BOOST_STATIC_H */
