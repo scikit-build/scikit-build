0e986db4f98531f3dc159ef8245ffe98f5afd6be
Omar Padron <omar.padron@kitware.com>
2016-06-22 12:59:54 -0400

    try adding test harness code

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -16,6 +16,9 @@ import sys
 from skbuild.exceptions import SKBuildError
 from skbuild.cmaker import SKBUILD_DIR, CMAKE_BUILD_DIR
 
+from .common import test_case
+
+@test_case
 def test_hello_builds():
     old_argv = sys.argv
     old_cwd = os.getcwd()
@@ -32,6 +35,7 @@ def test_hello_builds():
         os.chdir(old_cwd)
         sys.argv = old_argv
 
+@test_case
 def test_hello_works():
     old_cwd = os.getcwd()
     os.chdir(os.path.join("samples", "hello", CMAKE_BUILD_DIR))
