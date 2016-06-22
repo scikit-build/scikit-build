a5bb6eb0fa7f6f7b1eeda4bcf2531048885a1d71
Omar Padron <omar.padron@kitware.com>
2016-06-22 13:23:14 -0400

    give up on vtk testing in CI

--- tests/test_pen2.py
+++ tests/test_pen2.py
@@ -16,9 +16,6 @@ import sys
 from skbuild.exceptions import SKBuildError
 from skbuild.cmaker import SKBUILD_DIR, CMAKE_BUILD_DIR
 
-from .common import test_case
-
-@test_case
 def test_pen2_builds():
     old_argv = sys.argv
     old_cwd = os.getcwd()
@@ -35,7 +32,6 @@ def test_pen2_builds():
         os.chdir(old_cwd)
         sys.argv = old_argv
 
-@test_case
 def test_pen2_works():
     old_cwd = os.getcwd()
     os.chdir(os.path.join("samples", "pen2-cython", CMAKE_BUILD_DIR))
