fc16adb1f1a9be5deee6202d20c2741085047991
Omar Padron <omar.padron@kitware.com>
2016-06-27 10:31:39 -0400

    escape whitespace in cmd IF statement

--- ci/appveyor/run.cmd
+++ ci/appveyor/run.cmd
@@ -3,12 +3,12 @@
 
 :: @ECHO OFF
 
-IF x%SKIP%==x0 (
-    IF NOT x%CMAKE_GENERATOR:Visual Studio=%==x%CMAKE_GENERATOR% (
+IF "x%SKIP%==x0" (
+    IF NOT "x%CMAKE_GENERATOR:Visual Studio=%==x%CMAKE_GENERATOR%" (
         cmd /E:ON /V:ON /C ci\appveyor\run-with-visual-studio.cmd %*
     )
 
-    IF NOT x%CMAKE_GENERATOR:MinGW Makefiles=%==x%CMAKE_GENERATOR% (
+    IF NOT "x%CMAKE_GENERATOR:MinGW Makefiles=%==x%CMAKE_GENERATOR%" (
         cmd /E:ON /V:ON /C ci\appveyor\run-with-mingw.cmd %*
     )
 ) ELSE (
