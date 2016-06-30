0fc807812c396d4fcdb2b1d44bf7f1f7c29546da
Omar Padron <omar.padron@kitware.com>
2016-06-30 18:32:53 -0500

    add sample project: "pen2-cython"

---
+++ tests/samples/pen2-cython/setup.py
@@ -0,0 +1,15 @@
+from skbuild import setup
+
+setup(
+    name="pen2_cython",
+    version="0.0.1",
+    description="double pendulum simulation (cython version)",
+    author="skbuild team",
+    license="MIT",
+
+    packages=['pen2_cython'],
+    package_dir={
+        'pen2_cython': 'src'
+    },
+    scripts=['scripts/pen2_cython']
+)
