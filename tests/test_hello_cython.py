6d99ec0268f688f6c059dc6db6cbb55c3c9ecfa7
Omar Padron <omar.padron@kitware.com>
2016-06-30 17:25:53 -0500

    add sample project: "hello-cython"

---
+++ tests/test_hello_cython.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""test_hello_cython
+----------------------------------
+
+Tries to build and test the `hello-cython` sample project.
+"""
+
+import os
+import os.path
+import shutil
+import subprocess
+import sys
+
+from skbuild.exceptions import SKBuildError
+from skbuild.cmaker import SKBUILD_DIR, CMAKE_BUILD_DIR
+
+def test_hello_cython_builds():
+    old_argv = sys.argv
+    old_cwd = os.getcwd()
+
+    sys.argv = ["setup.py", "build"]
+    os.chdir(os.path.join("samples", "hello-cython"))
+
+    if os.path.exists(SKBUILD_DIR):
+        shutil.rmtree(SKBUILD_DIR)
+
+    try:
+        exec(open("setup.py").read())
+    finally:
+        os.chdir(old_cwd)
+        sys.argv = old_argv
+
+def test_hello_cython_works():
+    old_cwd = os.getcwd()
+    os.chdir(os.path.join("samples", "hello-cython", CMAKE_BUILD_DIR))
+    try:
+        subprocess.check_call(
+            ["ctest", "--build-config",
+                os.environ.get("SKBUILD_CMAKE_CONFIG", "Debug"),
+                "--output-on-failure"])
+    finally:
+        os.chdir(old_cwd)
+
