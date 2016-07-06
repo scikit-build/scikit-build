403c1eb154cf6bf245da5e2645a0fbfcb9c37b2e
Omar Padron <omar.padron@kitware.com>
2016-07-05 21:01:13 -0400

    fix issue with tower-of-babel tests failing when ran under anaconda

--- tests/test_tower_of_babel.py
+++ tests/test_tower_of_babel.py
@@ -35,11 +35,30 @@ def test_tbabel_builds():
 def test_tbabel_works():
     old_cwd = os.getcwd()
     os.chdir(os.path.join("samples", "tower-of-babel", CMAKE_BUILD_DIR))
+
+    env = os.environ
+    pp = env.get("PYTHONPATH", [])
+    if(pp):
+        pp = pp.split(os.pathsep)
+
+    pyversion = "python" + ".".join(str(x) for x in sys.version_info[:2])
+    pp.extend((
+        os.path.join(sys.prefix, "lib", pyversion),
+        os.path.join(sys.prefix, "lib", pyversion, "site-packages"),
+        os.path.join(sys.prefix, "lib", pyversion, "lib-dynload"),
+        os.getcwd()
+    ))
+
+    env["PYTHONPATH"] = os.pathsep.join(pp)
+
+    env["PYTHONHOME"] = env.get("PYTHONHOME", sys.prefix)
+
     try:
         subprocess.check_call(
             ["ctest", "--build-config",
                 os.environ.get("SKBUILD_CMAKE_CONFIG", "Debug"),
-                "--output-on-failure"])
+                "--output-on-failure"],
+            env=env)
     finally:
         os.chdir(old_cwd)
 
