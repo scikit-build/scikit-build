56d1d625e6df5cdb58928959329fc4784a204a4d
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-07-19 01:59:36 -0400

    flake8: Add exception for expected "UnusedImport"

--- skbuild/__init__.py
+++ skbuild/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .distutils_wrap import setup
+from .distutils_wrap import setup  # noqa: F401: UnusedImport
 
 __author__ = 'The scikit-build team'
 __email__ = 'scikit-build@googlegroups.com'
