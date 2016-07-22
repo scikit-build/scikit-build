scikit-build/scikit-build

commit 561fd531c094cb75d2e9589ea6823da553478d26

flake8: skbuild/cmaker: Reduce complexity introducing new functions
This commit re-factor "configure" function introducing the following
helper methods:

* get_python_include_dir
* get_python_library
* check_for_bad_installs

skbuild/cmaker.py
@@ -106,6 +106,41 @@ class CMaker(object):
         if not os.path.exists(DISTUTILS_INSTALL_DIR):
             os.makedirs(DISTUTILS_INSTALL_DIR)
 
+        python_version = CMaker.get_python_version()
+        python_include_dir = CMaker.get_python_include_dir(python_version)
+        python_library = CMaker.get_python_library(python_version)
+
+        cmd = ['cmake', os.getcwd(), '-G', generator_id,
+               '-DCMAKE_INSTALL_PREFIX:PATH={0}'.format(
+                    os.path.join(os.getcwd(), CMAKE_INSTALL_DIR)),
+               '-DPYTHON_EXECUTABLE:FILEPATH=' + sys.executable,
+               '-DPYTHON_VERSION_STRING:STRING=' + sys.version.split(' ')[0],
+               '-DPYTHON_INCLUDE_DIR:PATH=' + python_include_dir,
+               '-DPYTHON_LIBRARY:FILEPATH=' + python_library,
+               '-DSKBUILD:BOOL=TRUE',
+               "-DCMAKE_MODULE_PATH:PATH={}".format(
+                   os.path.dirname(__file__) + '/resources/cmake')
+               ]
+
+        cmd.extend(clargs)
+
+        cmd.extend(
+            filter(bool,
+                   shlex.split(os.environ.get("SKBUILD_CONFIGURE_OPTIONS", "")))
+        )
+
+        # changes dir to cmake_build and calls cmake's configure step
+        # to generate makefile
+        rtn = subprocess.check_call(cmd, cwd=CMAKE_BUILD_DIR)
+        if rtn != 0:
+            raise RuntimeError("Could not successfully configure "
+                               "your project. Please see CMake's "
+                               "output for more information.")
+
+        CMaker.check_for_bad_installs()
+
+    @staticmethod
+    def get_python_version():
         python_version = sysconfig.get_config_var('VERSION')
 
         if not python_version:
@@ -114,6 +149,10 @@ class CMaker(object):
         if not python_version:
             python_version = ".".join(map(str, sys.version_info[:2]))
 
+        return python_version
+
+    @staticmethod
+    def get_python_include_dir(python_version):
         # determine python include dir
         python_include_dir = sysconfig.get_config_var('INCLUDEPY')
 
@@ -123,43 +162,16 @@ class CMaker(object):
             os.path.join(python_include_dir, 'Python.h'))
 
         if python_include_dir is None or not found_python_h:
-            candidate_prefixes = []
 
-            try:
-                candidate_prefixes.append(
-                    os.path.dirname(sysconfig.get_config_var('INCLUDEPY')))
-            except:
-                pass
-
-            try:
-                candidate_prefixes.append(
-                    sysconfig.get_config_var('INCLUDEDIR'))
-            except:
-                pass
-
-            try:
-                candidate_prefixes.append(
-                    os.path.dirname(sysconfig.get_path('include')))
-            except:
-                pass
-
-            try:
-                candidate_prefixes.append(
-                    os.path.dirname(sysconfig.get_path('platinclude')))
-            except:
-                pass
-
-            try:
-                candidate_prefixes.append(
-                    os.path.join(sysconfig.get_python_inc(),
-                                 ".".join(map(str, sys.version_info[:2]))))
-            except:
-                pass
-
-            try:
-                candidate_prefixes.append(sysconfig.get_python_inc())
-            except:
-                pass
+            candidate_prefixes = [
+                os.path.dirname(sysconfig.get_config_var('INCLUDEPY')),
+                sysconfig.get_config_var('INCLUDEDIR'),
+                os.path.dirname(sysconfig.get_path('include')),
+                os.path.dirname(sysconfig.get_path('platinclude')),
+                os.path.join(sysconfig.get_python_inc(),
+                             ".".join(map(str, sys.version_info[:2]))),
+                sysconfig.get_python_inc()
+                ]
 
             candidate_prefixes = tuple(filter(bool, candidate_prefixes))
 
@@ -183,6 +195,10 @@ class CMaker(object):
 
         # TODO(opadron): what happens if we don't find an include directory?
 
+        return python_include_dir
+
+    @staticmethod
+    def get_python_library(python_version):
         # determine direct path to libpython
         python_library = sysconfig.get_config_var('LIBRARY')
 
@@ -240,33 +256,10 @@ class CMaker(object):
 
         # TODO(opadron): what happens if we don't find a libpython?
 
-        cmd = ['cmake', os.getcwd(), '-G', generator_id,
-               '-DCMAKE_INSTALL_PREFIX:PATH={0}'.format(
-                    os.path.join(os.getcwd(), CMAKE_INSTALL_DIR)),
-               '-DPYTHON_EXECUTABLE:FILEPATH=' + sys.executable,
-               '-DPYTHON_VERSION_STRING:STRING=' + sys.version.split(' ')[0],
-               '-DPYTHON_INCLUDE_DIR:PATH=' + python_include_dir,
-               '-DPYTHON_LIBRARY:FILEPATH=' + python_library,
-               '-DSKBUILD:BOOL=TRUE',
-               "-DCMAKE_MODULE_PATH:PATH={}".format(
-                   os.path.dirname(__file__) + '/resources/cmake')
-               ]
+        return python_library
 
-        cmd.extend(clargs)
-
-        cmd.extend(
-            filter(bool,
-                   shlex.split(os.environ.get("SKBUILD_CONFIGURE_OPTIONS", "")))
-        )
-
-        # changes dir to cmake_build and calls cmake's configure step
-        # to generate makefile
-        rtn = subprocess.check_call(cmd, cwd=CMAKE_BUILD_DIR)
-        if rtn != 0:
-            raise RuntimeError("Could not successfully configure "
-                               "your project. Please see CMake's "
-                               "output for more information.")
-
+    @staticmethod
+    def check_for_bad_installs():
         # Try to catch files that are meant to be installed outside the project
         # root before they are actually installed.  We can not wait for the
         # manifest, so we try to extract the information from the CMake build

commit 1c11225560cdefff592cdfd71c9d4f3777765606

cmaker: Reintroduce exceptions when using sysconfig
This commit fixes a regression introduced in 561fd53

It turns out that in case of invalid parameter, 'get_path' will raise
exceptions whereas 'get_config_var' will return None.

Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

skbuild/cmaker.py
@@ -158,29 +158,66 @@ class CMaker(object):
 
         return python_version
 
-    @staticmethod
+    # NOTE(opadron): The try-excepts raise the cyclomatic complexity, but we
+    # need them for this function.
+    @staticmethod  # noqa: C901: Complexity
     def get_python_include_dir(python_version):
         # determine python include dir
         python_include_dir = sysconfig.get_config_var('INCLUDEPY')
 
         # if Python.h not found (or python_include_dir is None), try to find a
         # suitable include dir
-        found_python_h = os.path.exists(
-            os.path.join(python_include_dir, 'Python.h'))
+        found_python_h = (
+            python_include_dir is not None or
+            os.path.exists(os.path.join(python_include_dir, 'Python.h'))
+        )
 
-        if python_include_dir is None or not found_python_h:
+        if not found_python_h:
+            candidate_prefixes = []
 
-            candidate_prefixes = [
-                os.path.dirname(sysconfig.get_config_var('INCLUDEPY')),
-                sysconfig.get_config_var('INCLUDEDIR'),
-                os.path.dirname(sysconfig.get_path('include')),
-                os.path.dirname(sysconfig.get_path('platinclude')),
-                os.path.join(sysconfig.get_python_inc(),
-                             ".".join(map(str, sys.version_info[:2]))),
-                sysconfig.get_python_inc()
-                ]
+            # NOTE(opadron): these possible prefixes must be guarded against
+            # AttributeErrors and KeyErrors because they each can throw on
+            # different platforms or even different builds on the same platform.
+            include_py = sysconfig.get_config_var('INCLUDEPY')
+            include_dir = sysconfig.get_config_var('INCLUDEDIR')
+            include = None
+            plat_include = None
+            python_inc = None
+            python_inc2 = None
 
-            candidate_prefixes = tuple(filter(bool, candidate_prefixes))
+            try:
+                include = sysconfig.get_path('include')
+            except (AttributeError, KeyError):
+                pass
+
+            try:
+                plat_include = sysconfig.get_path('platinclude')
+            except (AttributeError, KeyError):
+                pass
+
+            try:
+                python_inc = sysconfig.get_python_inc()
+            except AttributeError:
+                pass
+
+            if include_py is not None:
+                include_py = os.path.dirname(include_py)
+            if include is not None:
+                include = os.path.dirname(include)
+            if plat_include is not None:
+                plat_include = os.path.dirname(plat_include)
+            if python_inc is not None:
+                python_inc2 = os.path.join(
+                    python_inc, ".".join(map(str, sys.version_info[:2])))
+
+            candidate_prefixes = list(filter(bool, (
+                include_py,
+                include_dir,
+                include,
+                plat_include,
+                python_inc,
+                python_inc2,
+            )))
 
             candidate_versions = (python_version,)
             if python_version:
@@ -201,6 +238,7 @@ class CMaker(object):
                     break
 
         # TODO(opadron): what happens if we don't find an include directory?
+        #                Throw SKBuildError?
 
         return python_include_dir
 
@@ -338,3 +376,5 @@ class CMaker(object):
                                              "install_manifest.txt")
         with open(install_manifest_path, "r") as manifest:
             return [_remove_cwd_prefix(path) for path in manifest]
+
+        return []