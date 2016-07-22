88239c64d63158d1b5e8de1623a07b1432fc87fe
Omar Padron <omar.padron@kitware.com>
2016-07-21 21:17:00 -0400

    Refactor tests introducing reusable decorators. See #63, #78
    Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

--- tests/test_hello_cython.py
+++ tests/test_hello_cython.py
@@ -7,42 +7,19 @@
 Tries to build and test the `hello-cython` sample project.
 """
 
-import os
-import os.path
-import shutil
-import subprocess
-import sys
-
-from skbuild.cmaker import SKBUILD_DIR, CMAKE_BUILD_DIR
+from . import project_setup_py_test
 
 
+@project_setup_py_test(("samples", "hello-cython"), ["build"], clear_cache=True)
 def test_hello_cython_builds():
-    old_argv = sys.argv
-    old_cwd = os.getcwd()
-
-    sys.argv = ["setup.py", "build"]
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-    os.chdir(os.path.join(cur_dir, "samples", "hello-cython"))
-
-    if os.path.exists(SKBUILD_DIR):
-        shutil.rmtree(SKBUILD_DIR)
-
-    try:
-        with open("setup.py", "r") as fp:
-            exec(fp.read())
-    finally:
-        os.chdir(old_cwd)
-        sys.argv = old_argv
+    pass
 
 
-def test_hello_cython_works():
-    old_cwd = os.getcwd()
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-    os.chdir(os.path.join(cur_dir, "samples", "hello-cython", CMAKE_BUILD_DIR))
-    try:
-        subprocess.check_call(
-            ["ctest", "--build-config",
-                os.environ.get("SKBUILD_CMAKE_CONFIG", "Debug"),
-                "--output-on-failure"])
-    finally:
-        os.chdir(old_cwd)
+# @project_setup_py_test(("samples", "hello-cython"), ["test"])
+# def test_hello_cython_works():
+#     pass
+
+
+@project_setup_py_test(("samples", "hello-cython"), ["bdist_wheel"])
+def test_hello_cython_wheel():
+    pass
--- tests/test_outside_project_root.py
+++ tests/test_outside_project_root.py
@@ -8,34 +8,22 @@ Tries to build the `fail-outside-project-root` sample project.  Ensures that the
 attempt fails with an SKBuildError exception.
 """
 
-import os
-import os.path
-import shutil
-import sys
-
 from skbuild.exceptions import SKBuildError
-from skbuild.cmaker import SKBUILD_DIR
+
+from . import project_setup_py_test
 
 
-def test_outside_project_root_installs():
-    old_argv = sys.argv
-    old_cwd = os.getcwd()
-
-    sys.argv = ["setup.py", "install"]
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-    os.chdir(os.path.join(cur_dir, "samples", "fail-outside-project-root"))
-
-    if os.path.exists(SKBUILD_DIR):
-        shutil.rmtree(SKBUILD_DIR)
+def test_outside_project_root_fails():
+    @project_setup_py_test(("samples", "fail-outside-project-root"),
+                           ["install"],
+                           clear_cache=True)
+    def should_fail():
+        pass
 
     exception_thrown = False
     try:
-        with open("setup.py", "r") as fp:
-            exec(fp.read())
+        should_fail()
     except SKBuildError:
         exception_thrown = True
-    finally:
-        os.chdir(old_cwd)
-        sys.argv = old_argv
 
     assert exception_thrown
