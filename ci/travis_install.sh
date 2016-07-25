083d7823bdaae41f5b454bb342c7d2340035f61d
Max Smolens <max.smolens@kitware.com>
2016-07-25 11:26:29 -0400

    ci/travis: Install CMake for command-line use on OS X

--- ci/travis_install.sh
+++ ci/travis_install.sh
@@ -7,9 +7,29 @@ CMAKE_VERSION=3.5
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   CMAKE_OS="Darwin"
   PIP_EXTRA_ARGS="--user"
+
+  install_cmake () {
+    local prefix="/usr/local/bin"
+    echo "Removing any existing CMake in $prefix"
+    sudo rm -f \
+      "$prefix/cmake" \
+      "$prefix/cpack" \
+      "$prefix/cmake-gui" \
+      "$prefix/ccmake" \
+      "$prefix/ctest"
+    echo "Installing CMake in $prefix"
+    sudo "$1/CMake.app/Contents/bin/cmake-gui" --install
+  }
+
 else
   CMAKE_OS="Linux"
   PIP_EXTRA_ARGS=""
+
+  install_cmake () {
+    echo "Copying $1/* to /usr/local"
+    sudo rsync -avz "$1/" /usr/local
+  }
+
 fi
 
 CMAKE_NAME="cmake-$CMAKE_VERSION.0-$CMAKE_OS-x86_64"
@@ -22,8 +42,7 @@ wget --no-check-certificate --progress=dot \
 echo "Extracting $CMAKE_PACKAGE.tar.gz"
 tar xzf "$CMAKE_PACKAGE"
 
-echo "Copying $CMAKE_NAME/* to /usr/local"
-sudo rsync -avz "$CMAKE_NAME/" /usr/local
+install_cmake $CMAKE_NAME
 
 pip install -r requirements.txt ${PIP_EXTRA_ARGS}
 
