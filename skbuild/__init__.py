712bbc8c9708c74a7fbeda31bb7ee77cd43f37a5
Matt McCormick <matt@mmmccormick.com>
2016-07-26 23:39:33 -0400

    Bump version to 0.3.0 (#111)

--- skbuild/__init__.py
+++ skbuild/__init__.py
@@ -4,6 +4,6 @@ from .setuptools_wrap import setup  # noqa: F401
 
 __author__ = 'The scikit-build team'
 __email__ = 'scikit-build@googlegroups.com'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 __all__ = ["setup"]
