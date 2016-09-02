c3a2533c85010b132c9d9decca2bd9aaeacdd297
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-09-02 04:10:11 -0400

    tests: Add tests checking that broken projects fail. Fixes #137, fixes #138

---
+++ tests/samples/fail-with-fatal-error-cmakelists/setup.py
@@ -0,0 +1,10 @@
+from skbuild import setup
+
+setup(
+    name="fail_with_fatal_error_cmakelists",
+    version="0.0.1",
+    description=("test project that should always fail to build because it "
+                 "provides a CMakeLists.txt reporting a FATAL_ERROR"),
+    author="The scikit-build team",
+    license="MIT",
+)
---
+++ tests/samples/fail-with-syntax-error-cmakelists/setup.py
@@ -0,0 +1,10 @@
+from skbuild import setup
+
+setup(
+    name="fail_with_syntax_error_cmakelists",
+    version="0.0.1",
+    description=("test project that should always fail to build because it "
+                 "provides a CMakeLists.txt with a syntax error"),
+    author="The scikit-build team",
+    license="MIT",
+)
