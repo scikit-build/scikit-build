68d7aa15b8d27fab320d3da678517cb48f37534a
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-09-06 18:27:53 -0400

    skbuild/utils: Teach push_dir how to make directory
    Also introduce skbuild.utils.mkdir_p allowing to recursively create
    a directory and its parents.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,6 +9,8 @@ Tests for utils functions.
 
 import os
 
+from skbuild.utils import mkdir_p
+
 saved_cwd = os.getcwd()
 
 
@@ -24,7 +26,7 @@ def teardown_module():
     os.chdir(saved_cwd)
 
 
-def test_push_dir():
+def test_push_dir(tmpdir):
     assert os.path.split(os.getcwd())[-1] == 'tests'
 
     from skbuild.utils import push_dir
@@ -35,13 +37,31 @@ def test_push_dir():
         assert os.path.split(os.getcwd())[-1] == 'scikit-build'
     assert os.path.split(os.getcwd())[-1] == 'tests'
 
-    # With directory
+    # With existing directory
     with push_dir(directory=os.path.join(os.getcwd(), '..')):
         assert os.path.split(os.getcwd())[-1] == 'scikit-build'
     assert os.path.split(os.getcwd())[-1] == 'tests'
 
+    foo_directory = os.path.join(str(tmpdir), 'foo')
 
-def test_push_dir_decorator():
+    # With non existing directory
+    failed = False
+    try:
+        with push_dir(directory=foo_directory):
+            pass
+    except OSError:
+        failed = True
+    assert failed
+    assert not os.path.isdir(foo_directory)
+
+    # With make_directory option
+    with push_dir(directory=foo_directory, make_directory=True):
+        assert os.getcwd() == foo_directory
+    assert os.path.split(os.getcwd())[-1] == 'tests'
+    assert os.path.isdir(foo_directory)
+
+
+def test_push_dir_decorator(tmpdir):
     assert os.path.split(os.getcwd())[-1] == 'tests'
 
     from skbuild.utils.decorator import push_dir
@@ -55,10 +75,48 @@ def test_push_dir_decorator():
     test_default()
     assert os.path.split(os.getcwd())[-1] == 'tests'
 
-    # With directory
+    # With existing directory
     @push_dir(directory=os.path.join(os.getcwd(), '..'))
     def test():
         assert os.path.split(os.getcwd())[-1] == 'scikit-build'
 
     test()
     assert os.path.split(os.getcwd())[-1] == 'tests'
+
+    foo_directory = os.path.join(str(tmpdir), 'foo')
+
+    # With non existing directory
+    failed = False
+    try:
+        @push_dir(directory=foo_directory)
+        def test():
+            pass
+
+        test()
+    except OSError:
+        failed = True
+    assert failed
+    assert not os.path.isdir(foo_directory)
+
+    # With make_directory option
+    @push_dir(directory=foo_directory, make_directory=True)
+    def test():
+        assert os.getcwd() == foo_directory
+
+    test()
+    assert os.path.split(os.getcwd())[-1] == 'tests'
+    assert os.path.isdir(foo_directory)
+
+
+def test_mkdir_p(tmpdir):
+    tmp_dir = str(tmpdir)
+    assert os.path.isdir(tmp_dir)
+
+    foo_bar_dir = os.path.join(tmp_dir, 'foo', 'bar')
+
+    mkdir_p(foo_bar_dir)
+    assert os.path.isdir(foo_bar_dir)
+
+    # Make sure calling function twice does not raise an exception
+    mkdir_p(foo_bar_dir)
+    assert os.path.isdir(foo_bar_dir)
