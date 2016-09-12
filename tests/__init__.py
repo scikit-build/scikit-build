69cc794dbeed98ae0c5e16b1caddf0831084d062
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-09-12 01:26:38 -0400

    tests: Refactor code introducing 'initialize_git_repo_and_commit' function

--- tests/__init__.py
+++ tests/__init__.py
@@ -117,18 +117,34 @@ def _copy_dir(target_dir, entry, on_duplicate='exception', keep_top_dir=False):
             # on_duplicate == 'ignore': do nothing with e2
 
 
+def initialize_git_repo_and_commit(project_dir):
+    """Convenience function creating a git repository in ``project_dir``.
+
+    If ``project_dir`` does NOT contain a ``.git`` directory, a new
+    git repository with one commit containing all the directories and files
+    is created.
+    """
+    if project_dir.join('.git').exists():
+        return
+
+    with push_dir(str(project_dir)):
+        for cmd in [
+            ['git', 'init'],
+            ['git', 'config', 'user.name', 'scikit-build'],
+            ['git', 'config', 'user.email', 'test@test'],
+            ['git', 'add', '-A'],
+            ['git', 'commit', '-m', 'Initial commit']
+        ]:
+            subprocess.check_call(cmd)
+
+
 def prepare_project(project, tmp_project_dir):
     """Convenience function setting up the build directory ``tmp_project_dir``
     for the selected sample ``project``.
 
     If ``tmp_project_dir`` does not exist, it is created.
 
-    If ``tmp_project_dir`` is empty, the function does the following:
-
-        1. It recursively copies the sample ``project`` into it.
-
-        2. Initialize a new git repository with one commit containing
-        all the directories and files.
+    If ``tmp_project_dir`` is empty, the sample ``project`` is copied into it.
     """
 
     tmp_project_dir = py.path.local(tmp_project_dir)
@@ -139,20 +155,7 @@ def prepare_project(project, tmp_project_dir):
 
     # If empty, copy project files and initialize git
     if not tmp_project_dir.listdir():
-        src_project_dir = os.path.join(SAMPLES_DIR, project)
-
-        _copy_dir(tmp_project_dir, src_project_dir)
-
-        # Initialize git
-        with push_dir(str(tmp_project_dir)):
-            for cmd in [
-                ['git', 'init'],
-                ['git', 'config', 'user.name', 'scikit-build'],
-                ['git', 'config', 'user.email', 'test@test'],
-                ['git', 'add', '-A'],
-                ['git', 'commit', '-m', 'Initial commit']
-            ]:
-                subprocess.check_call(cmd)
+        _copy_dir(tmp_project_dir, os.path.join(SAMPLES_DIR, project))
 
 
 @contextmanager
@@ -186,6 +189,7 @@ def project_setup_py_test(project, setup_args, tmp_dir=None):
             if wrapped.tmp_dir is None:
                 wrapped.tmp_dir = _tmpdir(fun.__name__)
                 prepare_project(wrapped.project, wrapped.tmp_dir)
+                initialize_git_repo_and_commit(wrapped.tmp_dir)
 
             with execute_setup_py(wrapped.tmp_dir, wrapped.setup_args):
                 result2 = fun(*iargs, **ikwargs)
