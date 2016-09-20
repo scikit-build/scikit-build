1bc290f962c29cf570a65753f9c9313ac9ed634f
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-09-20 16:53:54 -0400

    Bump version to 0.4.0

--- skbuild/__init__.py
+++ skbuild/__init__.py
@@ -9,6 +9,6 @@ from .setuptools_wrap import setup  # noqa: F401
 
 __author__ = 'The scikit-build team'
 __email__ = 'scikit-build@googlegroups.com'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 __all__ = ["setup"]
