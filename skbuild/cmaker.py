scikit-build/scikit-build

commit b8f55721e5e05ff46f0b07593667a520c9d5e093

skbuild: Fix build within virtualenv on windows
Update cmaker to use distutils.sysconfig, this is the module "patched"
by virtualenv to avoid applying pypa/virtualenv#992.

Updates "pytest-virtualenv" minimum required version to 1.2.5
to include manahl/pytest-plugins#37

skbuild/cmaker.py
@@ -3,6 +3,7 @@ This module provides an interface for invoking CMake executable.
 """
 
 import argparse
+import distutils.sysconfig as sysconfig
 import glob
 import itertools
 import os
@@ -12,7 +13,6 @@ import re
 import subprocess
 import shlex
 import sys
-import sysconfig
 
 from .constants import (CMAKE_BUILD_DIR,
                         CMAKE_INSTALL_DIR,

tests/test_distribution.py
@@ -1,6 +1,5 @@
 
 import os
-import platform
 import pytest
 
 from path import Path
@@ -20,8 +19,6 @@ except ImportError:
 
 @pytest.mark.skipif(not HAS_PYTEST_VIRTUALENV,
                     reason="pytest_virtualenv not available. See #228")
-@pytest.mark.skipif(platform.system().lower() == "windows",
-                    reason="not yet supported. See PR#217")
 def test_source_distribution(virtualenv):
     sdists = Path(DIST_DIR).files(pattern="*.tar.gz")
     if not sdists:
@@ -39,8 +36,6 @@ def test_source_distribution(virtualenv):
 
 @pytest.mark.skipif(not HAS_PYTEST_VIRTUALENV,
                     reason="pytest_virtualenv not available. See #228")
-@pytest.mark.skipif(platform.system().lower() == "windows",
-                    reason="not yet supported. See PR#217")
 def test_wheel(virtualenv):
     wheels = Path(DIST_DIR).files(pattern="*.whl")
     if not wheels:

commit ea5ce2a885357515b718d61344dfa56cdea234e7

skbuild.cmaker: Fix cmaker.get_python_library on multi-arch Linux distribution
Because calling `sysconfig.get_config_var('multiarchsubdir')`
returns an empty string on Linux, `du_sysconfig` is only used to
get the value of `LIBDIR`.

This commit fixes a regression introduced in b8f5572 (skbuild: Fix build
within virtualenv on windows)

See #219

skbuild/cmaker.py
@@ -3,7 +3,7 @@ This module provides an interface for invoking CMake executable.
 """
 
 import argparse
-import distutils.sysconfig as sysconfig
+import distutils.sysconfig as du_sysconfig
 import glob
 import itertools
 import os
@@ -13,6 +13,7 @@ import re
 import subprocess
 import shlex
 import sys
+import sysconfig
 
 from .constants import (CMAKE_BUILD_DIR,
                         CMAKE_INSTALL_DIR,
@@ -289,7 +290,12 @@ class CMaker(object):
             if abiflags:
                 candidate_abiflags.append('')
 
-            libdir = sysconfig.get_config_var('LIBDIR')
+            # Ensure the value injected by virtualenv is
+            # returned on windows.
+            # Because calling `sysconfig.get_config_var('multiarchsubdir')`
+            # returns an empty string on Linux, `du_sysconfig` is only used to
+            # get the value of `LIBDIR`.
+            libdir = du_sysconfig.get_config_var('LIBDIR')
             if sysconfig.get_config_var('MULTIARCH'):
                 masd = sysconfig.get_config_var('multiarchsubdir')
                 if masd: