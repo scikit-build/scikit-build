6f86bb70654a49bc1e373a0cf9cf6e23f2a86f49
xoviat <xoviat@users.noreply.github.com>
2018-02-19 13:53:59 -0600

    specify SKBUILD_DIR with platform
    Set the build directory to depend on the Python
    version and system information, such that the build
    directory can be shared between different Python
    versions

--- skbuild/constants.py
+++ skbuild/constants.py
@@ -3,8 +3,14 @@ This module defines constants commonly used in scikit-build.
 """
 
 import os
+import sys
 
-SKBUILD_DIR = "_skbuild"
+from distutils.util import get_platform
+
+SKBUILD_DIR = os.path.join(
+    "_skbuild",
+    "{}-{}".format(get_platform(), '.'.join(map(str, sys.version_info[:2]))),
+)
 CMAKE_BUILD_DIR = os.path.join(SKBUILD_DIR, "cmake-build")
 CMAKE_INSTALL_DIR = os.path.join(SKBUILD_DIR, "cmake-install")
 SETUPTOOLS_INSTALL_DIR = os.path.join(SKBUILD_DIR, "setuptools")
