4b50f1d32c3077cae664ea32e1b18d39f0f88fef
xoviat <xoviat@users.noreply.github.com>
2018-02-26 20:12:50 -0600

    Filter nonexistant Python libraries
    Closes gh-255.

--- skbuild/resources/cmake/targetLinkLibrariesWithDynamicLookup.cmake
+++ skbuild/resources/cmake/targetLinkLibrariesWithDynamicLookup.cmake
@@ -550,6 +550,8 @@ function(target_link_libraries_with_dynamic_lookup target)
           list(APPEND link_props "${dynamic_lookup_flags}")
         endif()
       endif()
+    elseif(${lib} MATCHES "(debug|optimized|general)")
+      # See gh-255
     else()
       list(APPEND link_libs "${lib}")
     endif()
