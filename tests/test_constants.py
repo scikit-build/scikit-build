a569ce792890eb1a833711c3a14a8e27d08e0578
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2019-06-04 00:42:28 -0400

    Add tests for constants.set_skbuild_plat_name() and CMaker.test_check_for_bad_installs()

---
+++ tests/test_constants.py
@@ -0,0 +1,14 @@
+
+import os
+import sys
+
+from skbuild.constants import SKBUILD_DIR, set_skbuild_plat_name, skbuild_plat_name
+
+
+def test_set_skbuild_plat_name():
+    try:
+        previous_plat_name = skbuild_plat_name()
+        set_skbuild_plat_name("plat-name")
+        assert os.path.join("_skbuild", "plat-name-{}.{}".format(*sys.version_info[:2])) == SKBUILD_DIR()
+    finally:
+        set_skbuild_plat_name(previous_plat_name)
