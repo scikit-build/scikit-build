954fe6f97f3614e670401a2a2e56e0198a282bd6
joncrall <erotemic@gmail.com>
2019-07-28 22:08:12 -0400

    package works on linux now

--- tests/samples/hello-ctypes/hello/__init__.py
+++ tests/samples/hello-ctypes/hello/__init__.py
@@ -1,4 +1,8 @@
 import ctypes
+import sys
+from os.path import realpath
+from os.path import dirname
+from os.path import join
 
 
 def _get_lib_ext():
@@ -16,8 +20,19 @@ def _find_ctypes_lib(libname, root_dir):
     ext = _get_lib_ext()
     import glob
     # windows doesnt always start names with lib
-    candidates = list(glob.glob(join(root_dir, libname + '*' + ext)))
-    candidates += list(glob.glob(join(root_dir, 'lib' + libname + '*' + ext)))
+    print('Loading ctypes lib from root_dir = {!r}'.format(root_dir))
+    print('libname = {!r}'.format(libname))
+    candidates = []
+    if sys.platform.startswith('win32'):
+        candidates += list(glob.glob(join(root_dir, libname + '*' + ext)))
+    else:
+        candidates += list(glob.glob(join(root_dir, 'lib' + libname + '*' + ext)))
+
+    if len(candidates) == 0:
+        raise Exception('Unable to find ctype lib {} in {}'.format(libname, root_dir))
+    if len(candidates) > 1:
+        raise Exception('Too many matching libs: {}'.format(candidates))
+
     print('candidates = {!r}'.format(candidates))
     lib_fpath = candidates[0]
     return lib_fpath
@@ -25,7 +40,7 @@ def _find_ctypes_lib(libname, root_dir):
 
 def _load_ctypes_lib():
     root_dir = realpath(dirname(__file__))
-    libname = '_hello'
+    libname = 'hello'
     lib_fpath = _find_ctypes_lib(libname, root_dir)
 
     def def_cfunc(clib, return_type, func_name, arg_type_list):
