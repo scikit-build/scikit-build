a7c8486dbb22dde81e876758bf2b1e67ee52155d
Jon Crall <jon.crall@kitware.com>
2019-09-15 20:41:26 -0400

    Add helper utilities

--- skbuild/__init__.py
+++ skbuild/__init__.py
@@ -7,12 +7,13 @@ This module provides the *glue* between the setuptools Python module and CMake.
 
 from ._version import get_versions
 from .setuptools_wrap import setup  # noqa: F401
+from . import utils
 
 __author__ = 'The scikit-build team'
 __email__ = 'scikit-build@googlegroups.com'
 __version__ = '0.7.1'
 __version__ = get_versions()['version']
 
-__all__ = ["setup"]
 
+__all__ = ['setup', 'utils']
 del get_versions
