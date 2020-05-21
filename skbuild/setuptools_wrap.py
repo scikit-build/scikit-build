scikit-build/scikit-build

commit 66d7a3261c04f5a32e30ec54efb554b4daa489be

Add hook to process cmake install manifest

skbuild/setuptools_wrap.py
@@ -609,7 +609,13 @@ def setup(*args, **kw):  # noqa: C901
 
     package_prefixes = _collect_package_prefixes(package_dir, packages)
 
-    _classify_installed_files(cmkr.install(), package_data, package_prefixes,
+    # This hook enables custom processing of the cmake manifest
+    cmake_manifest = cmkr.install()
+    process_manifest = kw.get('cmake_process_manifest_hook')
+    if callable(process_manifest):
+        cmake_manifest = process_manifest(cmake_manifest)
+
+    _classify_installed_files(cmake_manifest, package_data, package_prefixes,
                               py_modules, new_py_modules,
                               scripts, new_scripts,
                               data_files,

commit e62bf6f89bf146bd826851c21332120ac7e6f04d

setuptools_wrap: Fix unknown setup options related to cmake_process_manifest_hook keyword
This commit addresses a regression introduced in 66d7a32  (Add hook to process
cmake install manifest) and prevents setuptools from complaining about unknown
setup options.

It removes cmake_process_manifest_hook keyword from argument dict.

Signed-off-by: Ievgen Popovych <ievgenp@seetrue.ai>

skbuild/setuptools_wrap.py
@@ -398,7 +398,8 @@ def setup(*args, **kw):  # noqa: C901
         'cmake_source_dir': '',
         'cmake_with_sdist': False,
         'cmake_languages': ('C', 'CXX'),
-        'cmake_minimum_required_version': None
+        'cmake_minimum_required_version': None,
+        'cmake_process_manifest_hook': None
     }
     skbuild_kw = {param: kw.pop(param, parameters[param])
                   for param in parameters}
@@ -611,7 +612,7 @@ def setup(*args, **kw):  # noqa: C901
 
     # This hook enables custom processing of the cmake manifest
     cmake_manifest = cmkr.install()
-    process_manifest = kw.get('cmake_process_manifest_hook')
+    process_manifest = skbuild_kw.get('cmake_process_manifest_hook')
     if process_manifest is not None:
         if callable(process_manifest):
             cmake_manifest = process_manifest(cmake_manifest)