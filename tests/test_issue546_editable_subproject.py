06cde93fea212cd856f3bfabc010b09c195b4cf9
Henry Schreiner <henryschreineriii@utexas.edu>
2021-08-23 12:27:01 -0700

    tests: fix windows

--- tests/test_issue546_editable_subproject.py
+++ tests/test_issue546_editable_subproject.py
@@ -5,4 +5,4 @@ DIR = os.path.abspath(os.path.dirname(__file__))
 
 def test_editable_subproject(virtualenv):
     virtualenv.run("pip install -e {DIR}/samples/issue-546-editable-subproject".format(DIR=DIR))
-    virtualenv.run("python -c 'import example.tools'")
+    virtualenv.run('python -c "import example.tools"')
