5146d3282a7b3abbb57e18ac41deb589017ab81f
Henry Schreiner <HenrySchreinerIII@gmail.com>
2022-02-02 12:14:10 -0500

    fix: conda cross-compile issue fix (#646)
    * fix: conda cross-compile issue fix
    
    * Update constants.py
    
    * fix: platform has a machine

--- skbuild/constants.py
+++ skbuild/constants.py
@@ -25,7 +25,10 @@ def _default_skbuild_plat_name():
     supported_macos_architectures = {"x86_64", "arm64"}
     macos_universal2_architectures = {"x86_64", "arm64"}
 
-    release, _, machine = platform.mac_ver()
+    # See https://github.com/scikit-build/scikit-build/issues/643 for a weird cross
+    # compiling bug that forces us to avoid getting machine from platform.mac_ver()[2]
+    release = platform.mac_ver()[0]
+    machine = platform.machine()
 
     # If the MACOSX_DEPLOYMENT_TARGET environment variable is defined, use
     # it, as it will be the most accurate. Otherwise use the value returned by
