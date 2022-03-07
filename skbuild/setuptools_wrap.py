d5d1cde82fec5c577e4f9b7d29de6b45458997f1
Vyas Ramasubramani <vyasr@nvidia.com>
2022-03-07 11:05:42 -0800

    Ensure that each package has a separate list of data elements.

--- skbuild/setuptools_wrap.py
+++ skbuild/setuptools_wrap.py
@@ -510,7 +510,10 @@ def setup(*args, **kw):  # noqa: C901
 
     packages = kw.get("packages", [])
     package_dir = kw.get("package_dir", {})
-    package_data = copy.deepcopy(kw.get("package_data", {}))
+    package_data = {
+        k: v.copy()
+        for k, v in kw.get("package_data", {}).items()
+    }
 
     py_modules = kw.get("py_modules", [])
     new_py_modules = {py_module: False for py_module in py_modules}
