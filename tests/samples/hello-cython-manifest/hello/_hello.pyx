d90c4cc0949beb055dc78ac5d5dfc222be1c112d
Henry Schreiner <HenrySchreinerIII@gmail.com>
2022-04-07 16:32:24 -0400

    Revert "fix: MANIFEST.in data install location (#682)" (#690)
    This reverts commit b732bc89d6e7944daa1848377d0cc178a78a83a7.

--- tests/samples/hello-cython-manifest/hello/_hello.pyx
+++
@@ -1,8 +0,0 @@
-
-cpdef void hello(str strArg):
-    "Prints back 'Hello <param>', for example example: hello.hello('you')"
-    print("Hello, {}! :)".format(strArg))
-
-cpdef long size():
-    "Returns elevation of Nevado Sajama."
-    return 21463L
