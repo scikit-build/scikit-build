fcdf4c8ceb197daa86214d7e06f98b4523485213
Henry Schreiner <henryschreineriii@gmail.com>
2022-05-18 14:50:38 -0400

    tests: enable cygwin skipped tests
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/test_numpy.py
+++ tests/test_numpy.py
@@ -3,14 +3,11 @@ import platform
 import subprocess
 import sys
 
-import pytest
-
 DIR = os.path.dirname(os.path.abspath(__file__))
 HELLO_NUMPY = os.path.join(DIR, "samples/hello-numpy")
 BASE = os.path.dirname(DIR)
 
 
-@pytest.mark.skipif(sys.platform.startswith("cygwin"), reason="Needs release of scikit-build to make cmake work")
 @pytest.mark.skipif(
     platform.python_implementation() == "PyPy" and sys.version_info >= (3, 9),
     reason="NumPy not released for PyPy 3.9 yet",
--- tests/test_pep518.py
+++ tests/test_pep518.py
@@ -2,19 +2,15 @@ import os
 import subprocess
 import sys
 
-import pytest
-
 DIR = os.path.dirname(os.path.abspath(__file__))
 HELLO_PEP518 = os.path.join(DIR, "samples/hello-pep518")
 BASE = os.path.dirname(DIR)
 
 
-@pytest.mark.skipif(sys.platform.startswith("cygwin"), reason="Needs release of scikit-build to make cmake work")
 def test_pep518(pep518):
     subprocess.check_call([sys.executable, "-m", "build", "--wheel"], cwd=HELLO_PEP518)
 
 
-@pytest.mark.skipif(sys.platform.startswith("cygwin"), reason="Needs release of scikit-build to make cmake work")
 def test_dual_pep518(pep518):
     subprocess.check_call([sys.executable, "-m", "build", "--wheel"], cwd=HELLO_PEP518)
     subprocess.check_call([sys.executable, "-m", "build", "--wheel"], cwd=HELLO_PEP518)
