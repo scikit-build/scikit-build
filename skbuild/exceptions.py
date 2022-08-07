1b88107c6da94e07cc22f2c8894883d972fb22f3
claudioperez <50180406+claudioperez@users.noreply.github.com>
2022-08-07 16:30:38 -0700

    Update exceptions.py

--- skbuild/exceptions.py
+++ skbuild/exceptions.py
@@ -8,6 +8,9 @@ class SKBuildError(RuntimeError):
     project.
     """
 
+class SKBuildInvalidFileInstallationError(SKBuildError):
+    """Exception raised when a file is being installed into an invalid location.
+    """
 
 class SKBuildGeneratorNotFoundError(SKBuildError):
     """Exception raised when no suitable generator is found for the current
