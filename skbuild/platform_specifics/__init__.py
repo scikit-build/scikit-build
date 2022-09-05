2ed7518eea14475fb55e06a57cfe587e4552ddd7
Henry Schreiner <henryschreineriii@gmail.com>
2022-09-05 11:58:30 -0400

    fix: be specific about platform data
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- skbuild/platform_specifics/__init__.py
+++ skbuild/platform_specifics/__init__.py
@@ -1,5 +1,8 @@
 """This package provides :func:`get_platform()` allowing to get an instance
 of :class:`.abstract.CMakePlatform` matching the current platform.
+
+This folder contains files the define CMake's defaults for given platforms.  Any of them can be overridden by either
+command line or by environment variables.
 """
 
 from .abstract import CMakeGenerator  # noqa: F401
