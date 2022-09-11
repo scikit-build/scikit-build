30091d6c7afc8423417a16def64b7df38854015c
Aaron Gokaslan <aaronGokaslan@gmail.com>
2022-09-11 18:12:32 -0400

    chore: use stdlib ContextDecorator and os.makedirs (#748)
    * Use stdlib ContextDecorator
    
    * Fix bugs
    
    * Remove context decorator tests

--- skbuild/utils/__init__.py
+++ skbuild/utils/__init__.py
@@ -1,11 +1,9 @@
 """This module defines functions generally useful in scikit-build."""
 
 import contextlib
-import errno
 import os
 from collections import namedtuple
-from contextlib import contextmanager
-from functools import wraps
+from contextlib import ContextDecorator, contextmanager
 
 from distutils.command.build_py import build_py as distutils_build_py
 from distutils.errors import DistutilsTemplateError
@@ -41,42 +39,11 @@ def _log_warning(msg, *args):
         print(msg % args)
 
 
-class ContextDecorator:
-    """A base class or mixin that enables context managers to work as
-    decorators."""
-
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
-    def __enter__(self):
-        # Note: Returning self means that in "with ... as x", x will be self
-        return self
-
-    def __exit__(self, typ, val, traceback):
-        pass
-
-    def __call__(self, func):
-        @wraps(func)
-        def inner(*args, **kwds):
-            with self:
-                return func(*args, **kwds)
-
-        return inner
-
-
 def mkdir_p(path):
     """Ensure directory ``path`` exists. If needed, parent directories
     are created.
-
-    Adapted from http://stackoverflow.com/a/600612/1539918
     """
-    try:
-        os.makedirs(path)
-    except OSError as exc:  # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else:  # pragma: no cover
-            raise
+    return os.makedirs(path, exist_ok=True)
 
 
 class push_dir(ContextDecorator):
@@ -91,16 +58,16 @@ class push_dir(ContextDecorator):
         :param make_directory:
           If True, ``directory`` is created.
         """
-        self.directory = None
-        self.make_directory = None
+        super().__init__()
+        self.directory = directory
+        self.make_directory = make_directory
         self.old_cwd = None
-        super().__init__(directory=directory, make_directory=make_directory)
 
     def __enter__(self):
         self.old_cwd = os.getcwd()
         if self.directory:
             if self.make_directory:
-                mkdir_p(self.directory)
+                os.makedirs(self.directory, exist_ok=True)
             os.chdir(self.directory)
         return self
 
