a9a5f94fe152215f0db6f7e637cb29a06343bfcb
Henry Schreiner <henryschreineriii@gmail.com>
2022-11-30 15:36:52 -0500

    tests: include printout of installed packages of interest
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,12 @@ import sys
 
 import pytest
 
+if sys.version_info < (3, 8):
+    import importlib_metadata as metadata
+else:
+    from importlib import metadata
+
+
 DIR = os.path.dirname(os.path.abspath(__file__))
 BASE = os.path.dirname(DIR)
 
@@ -43,3 +49,26 @@ def pep518(pep518_wheelhouse, monkeypatch):
     monkeypatch.setenv("PIP_FIND_LINKS", pep518_wheelhouse)
     monkeypatch.setenv("PIP_NO_INDEX", "true")
     return pep518_wheelhouse
+
+
+def pytest_report_header() -> str:
+    interesting_packages = {
+        "build",
+        "distro",
+        "packaging",
+        "pip",
+        "setuptools",
+        "virtualenv",
+        "wheel",
+    }
+    valid = []
+    for package in interesting_packages:
+        try:
+            version = metadata.version(package)  # type: ignore[no-untyped-call]
+        except ModuleNotFoundError:
+            continue
+        valid.append(f"{package}=={version}")
+    reqs = " ".join(sorted(valid))
+    pkg_line = f"installed packages of interest: {reqs}"
+
+    return "\n".join([pkg_line])
