7f46d3f9e1eec6bb432164ae7f76d62583e41b5f
Vyas Ramasubramani <vyasr@nvidia.com>
2023-02-25 18:36:41 -0800

    Move test into proper file.

---
+++ tests/test_issue707_tested_packages.py
@@ -0,0 +1,23 @@
+from . import (
+    project_setup_py_test,
+)
+
+
+
+def test_install_command(capfd):
+    @project_setup_py_test("issue-707-nested-packages", ["install"], disable_languages_test=True)
+    def build():
+        pass
+
+    with capfd.disabled():
+        build()
+
+    # Verify that both
+    import hello
+    hello.hello("World")
+    captured = capfd.readouterr()
+    assert captured.out == "Hello, World!\n"
+
+    hello.goodbye.goodbye("World")
+    captured = capfd.readouterr()
+    assert captured.out == "Goodbye, World!\n"
