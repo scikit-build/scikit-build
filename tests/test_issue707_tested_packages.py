9a1e83164e6baf406855f4bb3f2248584b031c57
Vyas Ramasubramani <vyasr@nvidia.com>
2023-02-25 20:33:50 -0800

    Switch to using build_ext --inplace.

--- tests/test_issue707_tested_packages.py
+++ tests/test_issue707_tested_packages.py
@@ -1,27 +1,24 @@
 from __future__ import annotations
 
-from skbuild.utils import push_dir
-
 from . import project_setup_py_test
 
 
-def test_install_command(capfd):
-    with push_dir():
+def test_install_command(capfd, monkeypatch):
+    @project_setup_py_test("issue-707-nested-packages", ["build_ext", "--inplace"], disable_languages_test=True)
+    def build():
+        pass
 
-        @project_setup_py_test("issue-707-nested-packages", ["install"], disable_languages_test=True)
-        def build():
-            pass
+    build()
+    capfd.readouterr()
+    monkeypatch.syspath_prepend(build.tmp_dir)
 
-        build()
-        capfd.readouterr()
+    # Verify that both
+    import hello_nested
 
-        # Verify that both
-        import hello_nested
+    hello_nested.hello("World")
+    captured = capfd.readouterr()
+    assert captured.out == "Hello, World!\n"
 
-        hello_nested.hello("World")
-        captured = capfd.readouterr()
-        assert captured.out == "Hello, World!\n"
-
-        hello_nested.goodbye_nested.goodbye("World")
-        captured = capfd.readouterr()
-        assert captured.out == "Goodbye, World!\n"
+    hello_nested.goodbye_nested.goodbye("World")
+    captured = capfd.readouterr()
+    assert captured.out == "Goodbye, World!\n"
