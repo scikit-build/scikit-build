6e72aad8af5347de0216d16d23301f5119b96fdc
Henry Schreiner <HenrySchreinerIII@gmail.com>
2023-03-03 16:25:59 -0500

    fix: user f2py get_include if present

--- skbuild/resources/cmake/FindF2PY.cmake
+++ skbuild/resources/cmake/FindF2PY.cmake
@@ -71,20 +71,20 @@ find_program(F2PY_EXECUTABLE NAMES f2py${PYTHON_VERSION_MAJOR} f2py)
 # XXX This is required to support NumPy < v0.15.0. See note in module documentation above.
 if(NOT F2PY_EXECUTABLE)
   find_package(NumPy)
-  set(F2PY_EXECUTABLE "${PYTHON_EXECUTABLE}" "-m" "numpy.f2py.__main__")
+  set(F2PY_EXECUTABLE "${PYTHON_EXECUTABLE}" "-m" "numpy.f2py")
 endif()
 
 if(NOT F2PY_INCLUDE_DIR)
   execute_process(
       COMMAND "${PYTHON_EXECUTABLE}"
-      -c "import os; from numpy import f2py; print(os.path.dirname(f2py.__file__))"
+      -c "import os; from numpy import f2py; print(f2py.get_include() if hasattr(f2py, 'get_include') else os.path.join(os.path.dirname(f2py.__file__), 'src'))"
       OUTPUT_VARIABLE _f2py_directory
       OUTPUT_STRIP_TRAILING_WHITESPACE
       ERROR_QUIET
   )
   string(REPLACE "\\" "/" _f2py_directory ${_f2py_directory})
 
-  set(F2PY_INCLUDE_DIR "${_f2py_directory}/src" CACHE STRING "F2PY source directory location" FORCE)
+  set(F2PY_INCLUDE_DIR "${_f2py_directory}" CACHE STRING "F2PY source directory location" FORCE)
 endif()
 
 # Set-up the F2PY libraries and include directories
