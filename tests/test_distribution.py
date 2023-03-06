04942aa5b07521e558f6bd7f721d88c9837d5132
Henry Schreiner <henryschreineriii@gmail.com>
2023-03-06 17:47:23 -0500

    tests: custom venv tester
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -2,43 +2,38 @@ from __future__ import annotations
 
 from pathlib import Path
 
-import pytest
-
 from . import initialize_git_repo_and_commit, prepare_project
 
 DIR = Path(__file__).parent.resolve()
-DIST_DIR = DIR.parent / "dist"
-
-# Test if package can be imported to allow testing on
-# conda-forge where ``pytest-virtualenv`` is not available.
-pytest.importorskip("pytest_virtualenv", reason="pytest_virtualenv not available. See #228")
 
 
-def test_source_distribution(virtualenv):
-    sdists = list(DIST_DIR.glob("*.tar.gz")) if DIST_DIR.exists() else []
-    if not sdists:
-        pytest.skip("no source distribution available")
-    assert len(sdists) == 1
+def test_source_distribution(virtualenv, tmp_path):
+    sdist_dir = tmp_path / "dist"
+    workspace = tmp_path / "workspace"
+    workspace.mkdir()
+    virtualenv.install("build")
+    virtualenv.module("build", "--sdist", "--outdir", sdist_dir, cwd=DIR.parent)
+    (sdist,) = sdist_dir.glob("*.tar.gz")
 
-    virtualenv.run(f"pip install {sdists[0]}")
-    assert "scikit-build" in virtualenv.installed_packages()
+    virtualenv.install(sdist)
 
-    prepare_project("hello-no-language", virtualenv.workspace, force=True)
-    initialize_git_repo_and_commit(virtualenv.workspace, verbose=False)
+    prepare_project("hello-no-language", str(workspace), force=True)
+    initialize_git_repo_and_commit(str(workspace), verbose=False)
 
-    virtualenv.run("python setup.py bdist_wheel")
+    virtualenv.run("python", "setup.py", "bdist_wheel", cwd=workspace)
 
 
-def test_wheel(virtualenv):
-    wheels = list(DIST_DIR.glob("*.whl")) if DIST_DIR.exists() else []
-    if not wheels:
-        pytest.skip("no wheel available")
-    assert len(wheels) == 1
+def test_wheel(virtualenv, tmp_path):
+    wheel_dir = tmp_path / "dist"
+    workspace = tmp_path / "workspace"
+    workspace.mkdir()
+    virtualenv.install("build")
+    virtualenv.module("build", "--wheel", "--outdir", wheel_dir, cwd=DIR.parent)
+    (wheel,) = wheel_dir.glob("*.whl")
 
-    virtualenv.run(f"pip install {wheels[0]}")
-    assert "scikit-build" in virtualenv.installed_packages()
+    virtualenv.install(wheel)
 
-    prepare_project("hello-no-language", virtualenv.workspace, force=True)
-    initialize_git_repo_and_commit(virtualenv.workspace, verbose=False)
+    prepare_project("hello-no-language", str(workspace), force=True)
+    initialize_git_repo_and_commit(str(workspace), verbose=False)
 
-    virtualenv.run("python setup.py bdist_wheel")
+    virtualenv.run("python", "setup.py", "bdist_wheel", cwd=workspace)
