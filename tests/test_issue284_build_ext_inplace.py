99ab719507c358b686324bacf85f128e8b09b89e
Henry Schreiner <henryschreineriii@gmail.com>
2023-04-03 13:34:43 -0400

    tests: xfail on macOS incorrect
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/test_issue284_build_ext_inplace.py
+++ tests/test_issue284_build_ext_inplace.py
@@ -1,16 +1,10 @@
 from __future__ import annotations
 
 import os
-import sys
-
-import pytest
 
 from . import get_ext_suffix, project_setup_py_test
 
-PYPY = hasattr(sys, "implementation") and sys.implementation.name == "pypy"
 
-
-@pytest.mark.xfail(PYPY and sys.platform.startswith("darwin"), reason="Broken on PyPy on macOS")
 @project_setup_py_test("issue-284-build-ext-inplace", ["build_ext", "--inplace"], disable_languages_test=True)
 def test_build_ext_inplace_command():
     assert os.path.exists(f"hello/_hello_sk{get_ext_suffix()}")
