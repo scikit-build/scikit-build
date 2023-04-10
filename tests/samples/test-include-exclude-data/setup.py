19de10246fc1584b41f1878ebefd0d6302027b65
Henry Schreiner <HenrySchreinerIII@gmail.com>
2023-04-10 10:21:25 -0700

    tests: update for setuptools explicit package listing requirement (#923)
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/samples/test-include-exclude-data/setup.py
+++ tests/samples/test-include-exclude-data/setup.py
@@ -6,12 +6,16 @@ setup(
     name="test_include_exclude_data",
     version="0.1.0",
     cmake_languages=(),
-    packages=["hello", "hello2"],
+    packages=["hello", "hello2", "hello.data.subdata", "hello2.data2.subdata2"],
     include_package_data=True,
     exclude_package_data={
-        "": [
-            "*/*/*_data4_include_from_manifest_and_exclude_from_setup.txt",
-            "*/*/*_data4_cmake_generated_and_exclude_from_setup.txt",
-        ]
+        "hello.data.subdata": [
+            "*_data4_cmake_generated_and_exclude_from_setup.txt",
+            "*data4_include_from_manifest_and_exclude_from_setup.txt",
+        ],
+        "hello2.data2.subdata2": [
+            "*_data4_cmake_generated_and_exclude_from_setup.txt",
+            "*_data4_include_from_manifest_and_exclude_from_setup.txt",
+        ],
     },
 )
