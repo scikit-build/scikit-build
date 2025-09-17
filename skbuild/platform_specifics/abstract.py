a82b3ab57d74b6c7b160d0224b3d38335d585a3f
Alexander Grund <Flamefire@users.noreply.github.com>
2025-09-17 17:00:50 +0200

    Fix failure with CMake 4
    CMake 4 removed compatibility with CMake 3.5 so no valid generator will be found as the test CMakeLists.txt causes an error.
    
    Use the current CMake version to set the required version to avoid this

--- skbuild/platform_specifics/abstract.py
+++ skbuild/platform_specifics/abstract.py
@@ -49,7 +49,7 @@ class CMakePlatform:
         if not os.path.exists(test_folder):
             os.makedirs(test_folder)
         with open(f"{test_folder}/CMakeLists.txt", "w", encoding="utf-8") as f:
-            f.write("cmake_minimum_required(VERSION 3.5)\n")
+            f.write("cmake_minimum_required(VERSION ${CMAKE_VERSION})\n")
             f.write("PROJECT(compiler_test NONE)\n")
             for language in languages:
                 f.write(f"ENABLE_LANGUAGE({language:s})\n")
